7022a91fbefa54c3babc2fd1c77d1e8a96184df9
Abhisek Das <abhid.cs@outlook.com>
2024-04-24 21:36:23 +0530

    third commit

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -4,3 +4,5 @@ const authController = require('../controllers/authController');
 const router = express.Router();
 
 router.post('/signup', authController.signUp);
+
+///This is to check git push is working or not
